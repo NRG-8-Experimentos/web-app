44668a872d94b54878a3c9066359cbcdf9a27368
Al3xiel <gabriel280104casas@gmail.com>
2025-09-25 11:25:05 -0500

    feat(invitations): added confirmation dialog to the accept invitation button.

--- src/app/invitations/components/invitation/invitation.component.ts
+++ src/app/invitations/components/invitation/invitation.component.ts
@@ -1,6 +1,8 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Invitation} from '../../model/invitation.entity';
 import {DetailsService} from '../../../shared/services/details.service';
+import {MatDialog} from '@angular/material/dialog';
+import {InvitationDialogComponent} from '../invitation-dialog/invitation-dialog.component';
 
 @Component({
   selector: 'app-invitation',
@@ -10,23 +12,32 @@ import {DetailsService} from '../../../shared/services/details.service';
 })
 export class InvitationComponent {
   constructor(
-    private detailsService: DetailsService
+    private detailsService: DetailsService,
+    private dialog: MatDialog
   ) {
   }
   @Input() invitation!: Invitation;
   @Output() invitationChanged = new EventEmitter<void>();
 
-  onAcceptRequest() {
-    this.detailsService.acceptOrDeclineInvitation(this.invitation.id, true)
-      .subscribe({
-        next: () => { this.invitationChanged.emit(); },
-        error: err => { /* mostrar error */ }
-      });  }
+
 
   onRejectRequest() {
     this.detailsService.acceptOrDeclineInvitation(this.invitation.id, false)
       .subscribe({
         next: () => { this.invitationChanged.emit(); },
         error: err => { /* mostrar error */ }
-      });  }
+      });
+  }
+
+  openDialog():void{
+    const dialogRef = this.dialog.open(InvitationDialogComponent, {
+      data: { invitation: this.invitation }
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      if (result) {
+        this.invitationChanged.emit(); // Actualiza la lista si se aceptó la invitación
+      }
+    });
+  }
 }
