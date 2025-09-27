9777dd1069aa8fdae78f0ad6cabecffa47dc9581
cwassointt <u202112078@upc.edu.pe>
2025-09-27 02:59:00 -0500

    feat: update analytics leader route to use new page component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,12 +10,12 @@ import {MembersLeaderComponent} from './groups/pages/members-leader/members-lead
 import {InvitationsLeaderComponent} from './invitations/pages/invitations-leader/invitations-leader.component';
 import {RequestsLeaderComponent} from './requests/pages/requests-leader/requests-leader.component';
 import {TasksLeaderComponent} from './tasks/pages/tasks-leader/tasks-leader.component';
-import {AnalyticsLeaderComponent} from './analytics/pages/analytics-leader/analytics-leader.component';
 import {MyGroupMemberComponent} from './groups/pages/my-group-member/my-group-member.component';
 import {TasksMemberComponent} from './tasks/pages/tasks-member/tasks-member.component';
 import {RequestsMemberComponent} from './requests/pages/requests-member/requests-member.component';
 import {AnalyticsMemberComponent} from './analytics/pages/analytics-member/analytics-member.component';
 import {InvitationMemberComponent} from './invitations/pages/invitation-member/invitation-member.component';
+import {AnalyticsLeaderPageComponent } from './analytics/pages/analytics-leader/analytics-leader-page.component';
 
 export const routes: Routes = [
   {path: '', component: MainComponent, canActivate:[authGuard]},
@@ -28,7 +28,7 @@ export const routes: Routes = [
   { path: 'leaders/my-group/invitations', component: InvitationsLeaderComponent },
   { path: 'leaders/my-group/request-&-validations', component: RequestsLeaderComponent },
   { path: 'leaders/my-group/tasks', component: TasksLeaderComponent },
-  { path: 'leaders/my-group/analytics', component: AnalyticsLeaderComponent },
+  { path: 'leaders/my-group/analytics', component: AnalyticsLeaderPageComponent },
   { path: 'members/my-group', component: MyGroupMemberComponent },
   { path: 'members/my-group/tasks', component: TasksMemberComponent },
   { path: 'members/my-group/request-&-validations', component: RequestsMemberComponent},
