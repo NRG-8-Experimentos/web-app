4605c046c854a0095459781cb88a83810b2ee5f2
cwassointt <u202112078@upc.edu.pe>
2025-09-27 09:51:11 -0500

    feat: rename analytics member component for consistency

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -13,9 +13,9 @@ import {TasksLeaderComponent} from './tasks/pages/tasks-leader/tasks-leader.comp
 import {MyGroupMemberComponent} from './groups/pages/my-group-member/my-group-member.component';
 import {TasksMemberComponent} from './tasks/pages/tasks-member/tasks-member.component';
 import {RequestsMemberComponent} from './requests/pages/requests-member/requests-member.component';
-import {AnalyticsMemberComponent} from './analytics/pages/analytics-member/analytics-member.component';
+import {AnalyticsMemberPageComponent} from './analytics/pages/analytics-member-page/analytics-member-page.component';
 import {InvitationMemberComponent} from './invitations/pages/invitation-member/invitation-member.component';
-import {AnalyticsLeaderPageComponent } from './analytics/pages/analytics-leader/analytics-leader-page.component';
+import {AnalyticsLeaderPageComponent } from './analytics/pages/analytics-leader-page/analytics-leader-page.component';
 
 export const routes: Routes = [
   {path: '', component: MainComponent, canActivate:[authGuard]},
@@ -32,7 +32,7 @@ export const routes: Routes = [
   { path: 'members/my-group', component: MyGroupMemberComponent },
   { path: 'members/my-group/tasks', component: TasksMemberComponent },
   { path: 'members/my-group/request-&-validations', component: RequestsMemberComponent},
-  { path: 'members/my-group/analytics', component: AnalyticsMemberComponent },
+  { path: 'members/my-group/analytics', component: AnalyticsMemberPageComponent },
   { path: 'members/group-search', component: InvitationMemberComponent },
   { path: '**', redirectTo: '' }
 ];
