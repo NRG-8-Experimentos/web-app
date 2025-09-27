403e3be141f104939dca1db8683ffd68e973ff96
cwassointt <u202112078@upc.edu.pe>
2025-09-27 02:59:51 -0500

    feat(analytics): add getMemberTasks and getTimePassedForMember methods to metrics service

--- src/app/analytics/services/metrics.service.ts
+++ src/app/analytics/services/metrics.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Observable, retry, catchError } from 'rxjs';
+import { Observable, retry, catchError, tap } from 'rxjs';
 import { BaseApiService } from '../../shared/services/base-api.service';
 import {
   TaskTimePassedResource,
@@ -20,64 +20,85 @@ export class MetricsService extends BaseApiService<any> {
 
   getAverageTaskTimePassed(memberId: number): Observable<TaskTimePassedResource> {
     return this.http.get<TaskTimePassedResource>(`${this.resourcePath()}/task/member/${memberId}/time-passed`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getTaskOverview(): Observable<TaskOverviewResource> {
     return this.http.get<TaskOverviewResource>(`${this.resourcePath()}/tasks/overview`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getTaskDistribution(): Observable<TaskDistributionResource> {
     return this.http.get<TaskDistributionResource>(`${this.resourcePath()}/tasks/distribution`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getRescheduledTasks(): Observable<RescheduledTasksResource> {
     return this.http.get<RescheduledTasksResource>(`${this.resourcePath()}/tasks/rescheduled`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getAvgCompletionTime(): Observable<AvgCompletionTimeResource> {
     return this.http.get<AvgCompletionTimeResource>(`${this.resourcePath()}/tasks/avg-completion-time`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getTaskOverviewForMember(memberId: number): Observable<TaskOverviewResource> {
     return this.http.get<TaskOverviewResource>(`${this.resourcePath()}/member/${memberId}/tasks/overview`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getTaskDistributionForMember(memberId: number): Observable<TaskDistributionResource> {
     return this.http.get<TaskDistributionResource>(`${this.resourcePath()}/member/${memberId}/tasks/distribution`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getRescheduledTasksForMember(memberId: number): Observable<RescheduledTasksResource> {
     return this.http.get<RescheduledTasksResource>(`${this.resourcePath()}/member/${memberId}/tasks/rescheduled`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
     );
   }
 
   getAvgCompletionTimeForMember(memberId: number): Observable<AvgCompletionTimeResource> {
     return this.http.get<AvgCompletionTimeResource>(`${this.resourcePath()}/member/${memberId}/tasks/avg-completion-time`, this.httpOptions).pipe(
-      retry(2),
-      catchError(this.handleError)
+        retry(2),
+        catchError(this.handleError)
+    );
+  }
+
+  /**
+   * Obtiene las tareas de un miembro desde api/v1/member/tasks
+   */
+  getMemberTasks(memberId: number): Observable<any> {
+    return this.http.get<any>(`/api/v1/member/tasks?memberId=${memberId}`, this.httpOptions).pipe(
+        retry(2),
+        tap(response => console.log('getMemberTasks response:', response)),
+        catchError(this.handleError)
+    );
+  }
+
+  getTimePassedForMember(memberId: number): Observable<TaskTimePassedResource> {
+    return this.http.get<TaskTimePassedResource>(
+        `${this.resourcePath()}/task/member/${memberId}/time-passed`,
+        this.httpOptions
+    ).pipe(
+        retry(2),
+        catchError(this.handleError)
     );
   }
 }
