095b2c4cb9fc603cbdb28d130e7b15ccbae760f9
cwassointt <u202112078@upc.edu.pe>
2025-09-27 02:59:43 -0500

    feat(analytics): add GroupsApiService for group member data retrieval

--- src/app/groups/services/groups-api.service.ts
+++ src/app/groups/services/groups-api.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import {BaseApiService} from '../../shared/services/base-api.service';
 import {Group} from '../model/group.entity';
 import {Observable} from 'rxjs';
+import {catchError, retry} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -18,5 +19,12 @@ export class GroupsApiService extends BaseApiService<Group>{
       params: { code }
     };
     return this.http.get<Group>(`${this.resourcePath()}/search`, options);
+  }
+
+  getGroupByMember(): Observable<Group> {
+    return this.http.get<Group>(`${this.resourcePath()}/member`, this.httpOptions).pipe(
+      retry(2),
+      catchError(this.handleError)
+    );
   }
 }
