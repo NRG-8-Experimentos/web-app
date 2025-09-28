1327163bcd0a5999aa169064c91bbe2760272b1e
NeoRise456 <defneorise@gmail.com>
2025-09-27 20:30:47 -0500

    feat: refactor GroupService to extend BaseApiService and add group-related methods

--- src/app/groups/services/group.service.ts
+++ src/app/groups/services/group.service.ts
@@ -1,30 +1,28 @@
 import { Injectable } from '@angular/core';
-import {environment} from '../../../environments/environment';
-import {HttpClient, HttpHeaders} from '@angular/common/http';
-import {Router} from '@angular/router';
-import {CreateGroupRequest} from '@app/groups/model/requests/create-group.request';
-import {CreateGroupResponse} from '@app/groups/model/response/create-group.response';
+import {BaseApiService} from '@app/shared/services/base-api.service';
+import {Group} from '@app/groups/model/group.entity';
+import {Observable} from 'rxjs';
+import {Task} from '@app/shared/model/task.entity';
+import {ShortMember} from '@app/shared/model/short-member.entity';
 
 @Injectable({
   providedIn: 'root'
 })
-export class GroupService {
-  basePath: string = `${environment.baseUrl}`;
-  httpOptions = { headers: new HttpHeaders({'Content-type': 'application/json'}) };
+export class GroupService extends BaseApiService<Group>{
+  constructor() {
+    super();
+    this.resourceEndPoint = '/groups';
+  }
 
-  constructor(private router: Router, private http: HttpClient) { }
+  getAllTasksOfGroup() : Observable<Task[]>{
+    return this.http.get<Task[]>(`${this.resourcePath()}`, this.httpOptions);
+  }
 
-  createGroup(createGroupRequest: CreateGroupRequest) {
-    //TODO: ADD THE TOKEN TO THE REQUEST
-    return this.http.post<CreateGroupResponse>(`${this.basePath}/leader/group`, createGroupRequest, this.httpOptions)
-      .subscribe({
-        next: (response) => {
-          this.router.navigate(['/groups']).then();
-        },
-        error: (error) => {
-          console.error(`Error while creating group: ${error}`);
-          this.router.navigate(['/create-group']).then();
-        }
-      });
+  searchGroupByCode(code: string): Observable<Group>{
+    return this.http.get<Group>(`${this.resourcePath()}/search?code=${code}`, this.httpOptions);
+  }
+
+  getAllMembersOfGroup(): Observable<ShortMember[]>{
+    return this.http.get<ShortMember[]>(`${this.resourcePath()}/members`, this.httpOptions);
   }
 }
