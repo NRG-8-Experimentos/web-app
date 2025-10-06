e165dbec0e9de687c6ba53fce198dadffd84e3aa
Racso24k <u20211g192@upc.edu.pe>
2025-10-06 00:01:58 -0500

    feat(members): refactor task entity and update task member details component for member-specific task retrieval

--- src/app/groups/model/task.entity.ts
+++ src/app/groups/model/task.entity.ts
@@ -1,21 +1,18 @@
-// @ts-ignore
-this.groupService.getAllTasksByGroupId(this.groupId).subscribe({
-  next: (tasksData: any[]) => {
-    // @ts-ignore
-    this.tasks = tasksData.map(task => new Task(
-      task.id,
-      task.title,
-      task.description,
-      task.dueDate,
-      task.createdAt,
-      task.updatedAt,
-      task.status,
-      task.member.id,
-      task.member.name,
-      task.member.surname,
-      task.member.urlImage,
-      task.groupId
-    ));
-  },
-  error: (err: any) => console.error('Error al obtener tareas:', err)
-});
+export class Task {
+  constructor(
+    public id: number,
+    public title: string,
+    public description: string,
+    public dueDate: string,
+    public createdAt: string,
+    public updatedAt: string,
+    public status: string,
+    public member: {
+      id: number;
+      name: string;
+      surname: string;
+      urlImage: string;
+    },
+    public groupId: number
+  ) {}
+}
